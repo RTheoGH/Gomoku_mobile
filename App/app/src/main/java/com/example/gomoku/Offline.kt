244651f596f6f56c1d9365337c73acf5f9a894a1
VigKillian <viguier.killian05@gmail.com>
2025-04-23 14:13:12 +0200

    Tour par tour offline + condition de victoire

--- App/app/src/main/java/com/example/gomoku/Offline.kt
+++ App/app/src/main/java/com/example/gomoku/Offline.kt
@@ -13,6 +13,8 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -25,6 +27,7 @@ import androidx.navigation.NavHostController
 fun Offline_lobby(pad : PaddingValues, navController: NavHostController){
     var player1 by remember { mutableStateOf("") }
     var player2 by remember { mutableStateOf("") }
+
 
     Column(
         modifier = Modifier.fillMaxSize().padding(pad).padding(8.dp)
@@ -68,6 +71,16 @@ fun Offline_lobby(pad : PaddingValues, navController: NavHostController){
 
 @Composable
 fun Offline_game(pad : PaddingValues, navController: NavHostController){
+    val size = 15
+    var playerTurn by remember { mutableIntStateOf(0) }
+    var board = remember {
+        mutableStateListOf<List<GomokuCell>>().apply{
+            for (i in 0 until size){
+                add(List(size){ j -> GomokuCell(i, j) })
+            }
+        }
+    }
+
     Column(
         modifier = Modifier.fillMaxSize().padding(pad).padding(8.dp)
     ){
@@ -81,7 +94,27 @@ fun Offline_game(pad : PaddingValues, navController: NavHostController){
             verticalArrangement = Arrangement.Center
         ) {
             Custom_row(1,"02:00","Joueur 1")
-            board()
+            Board(
+                board = board,
+                playerTurn = playerTurn,
+                onCellClick = { x, y ->
+                    if (board[x][y].state == CellState.EMPTY) {
+                        val newState = if (playerTurn == 0) CellState.BLACK else CellState.WHITE
+                        board[x] = board[x].toMutableList().apply {
+                            this[y] = board[x][y].copy(state = newState)
+                        }
+
+                        println(board[x][y])
+
+                        if (check_win(board, x, y, size)) {
+                            println("gagné !!!!!!")
+                        }
+
+                        playerTurn = 1 - playerTurn
+                        println("Tour du joueur : $playerTurn")
+                    }
+                }
+            )
             Custom_row(2,"02:00","Joueur 2")
         }
     }
